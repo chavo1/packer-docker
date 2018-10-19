182728facf4beff9eed91deec5a3280ba0517add
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-10-19 10:42:56 +0300

    Update check_ng.rb

--- test/integration/default/controls/check_ng.rb
+++ test/integration/default/controls/check_ng.rb
@@ -2,7 +2,15 @@ control 'check_nginx' do
 
   describe package('nginx') do
     it { should be_installed }
-  end
-
+end
+describe file("/etc/nginx/sites-available/default") do
+  it { should exist } # the configuration file should exist
+end
+describe nginx do
+  its('support_info') { should match /TLS/ }
+end
+describe nginx do
+  its('version') { should eq '1.14.0' }
+end
 end
 
